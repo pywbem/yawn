5f766954c7da5f87c5bd165a1187f6b6a628b465
Michal Minar <miminar@redhat.com>
2012-11-15 12:48:24 +0000

    fixed installation of static files for jqyeru-ui
    git-svn-id: https://svn.code.sf.net/p/pywbem/code/yawn/trunk@605 84f79d66-d411-0410-a85a-a3183736c6e8

--- mod_wsgi/setup.py
+++ mod_wsgi/setup.py
@@ -3,11 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
         name = "yawn",
-        version = "0.1.4",
+        version = "0.1.5",
         packages = find_packages(),
         package_data = { 'pywbem_yawn' :
             ['templates/*.mako', 'templates/*.txt'
-            , 'static/*.css', 'static/*.js', 'static/ui-lightness'] },
+            , 'static/*.css', 'static/*.js'
+            , 'static/ui-lightness/*.css'
+            , 'static/ui-lightness/images/*.png'] },
         scripts = ['scripts/yawn.py'],
         author = "Michal Minar",
         author_email = "miminar@redhat.com",
